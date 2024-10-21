126544e19fef96fc46dd05d184c999668aea3fbd
K-0bby <k.hoenyefia@gmail.com>
2024-10-21 15:15:17 +0000

    final update

--- script.js
+++ script.js
@@ -12,16 +12,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+const resetGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-let playing = true;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+resetGame();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -67,7 +78,7 @@ btnHold.addEventListener("click", function () {
     if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
-      diceEl.classList.add('hidden')
+      diceEl.classList.add("hidden");
 
       document
         .querySelector(`.player--${activePlayer}`)
@@ -75,9 +86,11 @@ btnHold.addEventListener("click", function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove("player--active");
+    } else {
+      // switch to next player
+      switchPlayer();
     }
-
-    // switch to next player
-    switchPlayer();
   }
 });
+
+btnNew.addEventListener("click", resetGame);
